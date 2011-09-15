b5abd899eb14a4dbc3d0439130253300ad9f9a4c
Maximilian Gaß <mxey@reaver.(none)>
2011-09-15 23:28:41 +0200

    Switch from unittest to py.test
    Thanks to timo, looks much cooler now

--- test/test.py
+++ test/test.py
@@ -1,35 +1,18 @@
-import unittest
 from subprocess import check_output, call
 import os
 
-class SSHRestrictTestCase(unittest.TestCase):
-    def test_simple(self):
-        os.environ["SSH_ORIGINAL_COMMAND"] =  "simple"
-        self.assertEqual(
-            "TEST_SIMPLE\n",
-            check_output(["./ssh-restrict", "./test/config"])
-        )
+def test_simple():
+    os.environ["SSH_ORIGINAL_COMMAND"] =  "simple"
+    assert "TEST_SIMPLE\n" == check_output(["./ssh-restrict", "./test/config"])
 
-    def test_args(self):
-        os.environ["SSH_ORIGINAL_COMMAND"] =  "args 23 42"
-        self.assertEqual(
-            "23 42\n", 
-            check_output(["./ssh-restrict", "./test/config"])
-        )
+def test_args():
+    os.environ["SSH_ORIGINAL_COMMAND"] =  "args 23 42"
+    assert "23 42\n" == check_output(["./ssh-restrict", "./test/config"])
 
-    def test_undefined(self):
-        os.environ["SSH_ORIGINAL_COMMAND"] =  "undefined"
-        self.assertEqual(
-            call(["./ssh-restrict", "./test/config"]),
-            1
-        )
+def test_undefined():
+    os.environ["SSH_ORIGINAL_COMMAND"] =  "undefined"
+    assert call(["./ssh-restrict", "./test/config"]) == 1
 
-    def test_notfound(self):
-        os.environ["SSH_ORIGINAL_COMMAND"] =  "notfound"
-        self.assertEqual(
-            call(["./ssh-restrict", "./test/config"]),
-            127
-        )
-
-if __name__ == '__main__':
-    unittest.main()
+def test_notfound():
+    os.environ["SSH_ORIGINAL_COMMAND"] =  "notfound"
+    assert call(["./ssh-restrict", "./test/config"]) == 127
