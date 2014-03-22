f5edcd79b8c982e0cf54420bf41a51fdc06516c2
Armin Pech <mail@arminpech.de>
2014-03-22 11:33:52 +0100

    Revert "Extended test cases; python3 support"
    This reverts commit 38c19147e9b7f21eda55bf5799e15049f9dd25e5.

--- test/test.py
+++ test/test.py
@@ -1,38 +1,18 @@
 from subprocess import check_output, call
 import os
 
-# Expected return codes
-return_code_unknown = 3
-return_code_notfound = return_code_unknown
-
 def test_simple():
     os.environ["SSH_ORIGINAL_COMMAND"] =  "simple"
-    assert b"TEST_SIMPLE\n" == check_output(["./ssh-restrict", "./test/config"])
+    assert "TEST_SIMPLE\n" == check_output(["./ssh-restrict", "./test/config"])
 
 def test_args():
     os.environ["SSH_ORIGINAL_COMMAND"] =  "args 23 42"
-    assert b"23 42\n" == check_output(["./ssh-restrict", "./test/config"])
+    assert "23 42\n" == check_output(["./ssh-restrict", "./test/config"])
 
 def test_undefined():
     os.environ["SSH_ORIGINAL_COMMAND"] =  "undefined"
-    assert call(["./ssh-restrict", "./test/config"]) == return_code_unknown
+    assert call(["./ssh-restrict", "./test/config"]) == 1
 
 def test_notfound():
     os.environ["SSH_ORIGINAL_COMMAND"] =  "notfound"
-    assert call(["./ssh-restrict", "./test/config"]) == return_code_notfound
-
-def test_fail_def():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "fail_def test"
-    assert call(["./ssh-restrict", "./test/config"]) == return_code_unknown
-
-def test_fail_com():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "fail_com 123"
-    assert call(["./ssh-restrict", "./test/config"]) == return_code_unknown
-
-def test_fail_eval():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "print_int notint"
-    assert call(["./ssh-restrict", "./test/config"]) == return_code_unknown
-
-def test_match():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "match"
-    assert b"match2\n" == check_output(["./ssh-restrict", "./test/config"])
+    assert call(["./ssh-restrict", "./test/config"]) == 127
