a501fac9e5b6d88dcadd98f81a5784342bc276a3
Maximilian Gaß <mxey@mxey.net>
2014-03-23 18:49:46 +0000

    Improve code formatting

--- test/test.py
+++ test/test.py
@@ -1,38 +1,48 @@
 from subprocess import check_output, call
+
 import os
+
 
 EXPECTED_RETURN_CODE_WRONG_USAGE = 3
 EXPECTED_RETURN_CODE_COMMAND_NOT_FOUND = 3
 EXPECTED_RETURN_CODE_INTERNAL_ERROR = 3
 
+
 def test_simple():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "simple"
+    os.environ["SSH_ORIGINAL_COMMAND"] = "simple"
     assert b"TEST_SIMPLE\n" == check_output(["./ssh-restrict", "./test/config"])
 
+
 def test_args():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "args 23 42"
+    os.environ["SSH_ORIGINAL_COMMAND"] = "args 23 42"
     assert b"23 42\n" == check_output(["./ssh-restrict", "./test/config"])
 
+
 def test_args_type_overwriting():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "args text 42"
+    os.environ["SSH_ORIGINAL_COMMAND"] = "args text 42"
     assert b"text 42\n" == check_output(["./ssh-restrict", "./test/config"])
 
+
 def test_undefined():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "undefined"
+    os.environ["SSH_ORIGINAL_COMMAND"] = "undefined"
     assert call(["./ssh-restrict", "./test/config"]) == EXPECTED_RETURN_CODE_COMMAND_NOT_FOUND
 
+
 def test_notfound():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "notfound"
+    os.environ["SSH_ORIGINAL_COMMAND"] = "notfound"
     assert call(["./ssh-restrict", "./test/config"]) == EXPECTED_RETURN_CODE_WRONG_USAGE
 
+
 def test_regex_exception():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "badregex test"
+    os.environ["SSH_ORIGINAL_COMMAND"] = "badregex test"
     assert call(["./ssh-restrict", "./test/config"]) == EXPECTED_RETURN_CODE_INTERNAL_ERROR
+
 
 def test_escape_exception():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "badescape"
+    os.environ["SSH_ORIGINAL_COMMAND"] = "badescape"
     assert call(["./ssh-restrict", "./test/config"]) == EXPECTED_RETURN_CODE_INTERNAL_ERROR
 
+
 def test_command_formatting():
-    os.environ["SSH_ORIGINAL_COMMAND"] =  "badformatting 123"
+    os.environ["SSH_ORIGINAL_COMMAND"] = "badformatting 123"
     assert call(["./ssh-restrict", "./test/config"]) == EXPECTED_RETURN_CODE_INTERNAL_ERROR
